db8055161819e85b75594a542aa074db9c57806f
MartinBINARD <martin.binard.dev@gmail.com>
2023-09-20 14:42:13 +0200

    React-router-dom : Add NavLinks

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/alien-green.svg';
+
+import './Header.scss';
 
 export default function Header() {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <Link to="/">Home</Link>
-      <Link to="/register">Register</Link>
+      <div className="nav">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'nav-link nav-link--active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/register"
+          className={({ isActive }) =>
+            isActive ? 'nav-link nav-link--active' : 'nav-link'
+          }
+        >
+          Register
+        </NavLink>
+      </div>
     </header>
   );
 }
